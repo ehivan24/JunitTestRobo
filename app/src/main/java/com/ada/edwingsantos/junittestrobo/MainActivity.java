b614b0854af83b6f915be8dd832b661ded9f7aaf
Hivan <ehivan24@hotmail.com>
2016-03-27 14:40:31 -0400

    Testing for intents and Click Listeners

--- app/src/main/java/com/ada/edwingsantos/junittestrobo/MainActivity.java
+++ app/src/main/java/com/ada/edwingsantos/junittestrobo/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,5 +19,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text_view1);
         textView.setText("HLLO");
+        Button btn = (Button) findViewById(R.id.btn_login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        });
+
     }
 }
--- app/src/test/java/com/ada/edwingsantos/junittestrobo/MainActivityTest.java
+++ app/src/test/java/com/ada/edwingsantos/junittestrobo/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.ada.edwingsantos.junittestrobo;
 
+import android.content.Intent;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.junit.Before;
@@ -8,25 +10,61 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.internal.Shadow;
+import org.robolectric.shadows.ShadowActivity;
 
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+//@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@Config(constants = BuildConfig.class, sdk = 21)
+
 public class MainActivityTest {
 
     private MainActivity activity;
     @Before
     public void setup(){
-        activity = Robolectric.setupActivity(MainActivity.class);
+        //activity = Robolectric.setupActivity(MainActivity.class);
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
     }
     @Test
     public void validateTextViewContent(){
+        /**
+         * This test will fail
+         */
         TextView textView = (TextView) activity.findViewById(R.id.text_view1);
         assertNotNull("TextView Not Exists", textView);
-        assertTrue("TextView ", "HELLO".equals(textView.getText().toString()));
+        assertTrue("TextView ", "HLLO".equals(textView.getText().toString()));
+    }
+    @Test
+    public void logInBtn(){
+        /**
+         * This test will pass
+         */
 
+        Button btn = (Button) activity.findViewById(R.id.btn_login);
+        assertNotNull("Button Exits", btn);
+        assertNotNull("Button", btn);
+        assertTrue("Button", "Logins".equals(btn.getText().toString()));
     }
 
+    @Test
+    public void testForBtnClick(){
+        /**
+         *  android {
+         *       useLibrary 'org.apache.http.legacy'
+         * }
+         *
+         *  Test for the Btn to open an Activity
+         *
+         */
+
+        activity.findViewById(R.id.btn_login).performClick();
+        Intent expectedIntent = new Intent(activity, LoginActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
 }
